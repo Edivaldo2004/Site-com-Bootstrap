db4b2afb555cc3527aa1e5e1189b964101f766fb
Edivaldo <edivaldosantosjunior2004@gmail.com>
2022-09-19 15:18:04 -0400

    Commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 //Configuração do HandleBars
 
@@ -11,16 +12,36 @@ app.engine('hbs', hbs.engine({
 }));
 app.set('view engine','hbs');
 
-app.use( express.static('public'))
+app.use( express.static('public'));
 
+app.use(bodyParser.urlencoded({extended:false}));
 
 //rota inicial
 //renderiza o home.hbs para abrir dentro da tag {{{body}} no layout
 app.get('/',(req, res)=> {
     res.render('home');
 });
+app.get('/cad_users',(req, res) => {
+    res.render('cad_users');
+});
 
+// rota renderizada
+app.get('/editar_users',(req, res) => {
+    res.render('editar_users');
+})
+
+// rota renderizada
+app.get('/exibir_users',(req, res) => {
+    res.render('exibir_users');
+})
+app.post('/insert_users',(req, res) => {
+    console.log(req.body);
+    res.send("Pegando");
+})
 // ativar o sistema
 app.listen(PORT,()=>{
     console.log('Servidor rodando em http:localhost:' + PORT);
 })
+ 
+
+
---
+++ models/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('bd_loja', 'root', ' ',{
+host: '127.0.0.1',
+dialect: 'mysql',
+define: {
+    charset: "utf8",
+    collate: 'utf8_general_ci',
+    timestamps: true
+},
+logging: false
+})
+
+sequelize.authenticate().then(function(){
+    console.log("Conectado ao banco");
+}).catch(function(err){
+    console.log("Falha no banco")
+})
